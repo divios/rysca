5329ea7a99f94ad03a2e670586cb72a02c626214
Divios <serberpar@gmail.com>
2020-09-25 11:28:01 +0200

    Terminado el arp_resolve

--- arp.c
+++ arp.c
@@ -6,8 +6,17 @@
 #include <libgen.h>
 #include <timerms.h>
 
-#define ARP_TYPE 0x0806
-#define IP_CONSTANT 0x0800
+
+#define IP_PROTOCOL 0x0800 //especificamos protocolo ip
+#define HARDW_TYPE = 0x0001 //especificamos que el hardware es eth
+
+#define ARP_TYPE 0x0800 //especificamos que el mensaje es de tipo ARP
+#define ARP_REQUEST = 0x0001 //simbolo para ARP request
+#define ARP_REPLY = 0x0002 //simbolo para ARP reply
+
+extern mac_addr_t MAC_BCAST_ADDR; //mac de broadcast
+#define UNKNOW_MAC = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; //cuando no sabemos la mac a utilizar
+
 
 timerms_t timer;
 long int timeout = 5000; //5segundos
@@ -30,40 +39,40 @@ typedef struct arp_message {
 int arp_resolve(eth_iface_t *iface, ipv4_addr_t destino, mac_addr_t mac) {
 
     arp_message_t arp_payload;
-    arp_payload.hard_addr = 1;// correspondiente a etf
-    arp_payload.protocol_type = IP_CONSTANT; //correspondiente a ip
+    arp_payload.hard_addr = HARDW_TYPE;// correspondiente a eth
+    arp_payload.protocol_type = IP_PROTOCOL; //correspondiente a ip
     arp_payload.hard_size = 6;//pq eth tiene 6 octetos
     arp_payload.protocol_length = 4;
-    arp_payload.opcode = 1; //1 request; 2 reply
-    arp_payload.mac_sender = mac;
-    arp_payload.ip_sender = {0, 0, 0, 0};
-    arp_payload.mac_target = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    arp_payload.opcode = ARP_REQUEST; //1 request; 2 reply
+    eth_getaddr(iface, arp_payload.mac_sender); //guardamos en mac_send la mac de la interfaz abierta
+    arp_payload.ip_sender = {0, 0, 0, 0}; //hastq que no implementemos la capa ip dejamos esto a 0
+    arp_payload.mac_target = UNKNOW_MAC;
     arp_payload.ip_target = destino;
 
 
-    if (eth_send(iface, destino, ARP_TYPE, (unsigned char *) &arp_payload, sizeof(arp_payload)) == -1) {
+    if (eth_send(iface, MAC_BCAST_ADDR, ARP_TYPE, (unsigned char *) &arp_payload, sizeof(arp_payload)) ==
+        -1) { //enviamos en broadcast un arp request
         return -1; //si no se ha podido enviar retornamos -1
     }
 
-    arp_message_t buffer; //nos hemos quedado aquí
-    int resolvedMac = -1;
+    unsigned char buffer; //nos hemos quedado aquí
 
     timerms_reset(&timer, timeout); //arrancamos el timer
 
     while (timerms_left(&timer) != 0) {
 
-        eth_recv(iface, mac, ARP_TYPE, buffer, sizeof(struct arp_message_t),
+        eth_recv(iface, arp_payload.mac_sender, ARP_TYPE, buffer, sizeof(struct arp_message_t),
                  timerms_left(&timer)); //solo recibimos si el mensaje es del tipo arp y esta dirigido a nuestra mac
 
         buffer = (struct arp_message) buffer; //eth_recv nos devuelve del tipo undefined char, asi que convertimos (no estoy muy seguro de esto)
 
         if (buffer.ip_sender == destino &&
             buffer.opcode == 2) {  //comprobamos que proviene de la ip que buscamos y ademas es arp reply
-            resolvedMac = buffer.mac_sender;  //todo esto se puede hacer con memcpy...
+            mac = buffer.mac_sender;  //todo esto se puede hacer con memcpy...
             break;
         }
     }
 
-    return resolvedMac;
+    return 1;
 
 }
--- arp_client.c
+++ arp_client.c
@@ -9,3 +9,19 @@
 #include <timerms.h>
 
 
+//generar fichero con el cache de ARP
+//Inicializarlo a 0??
+
+
+int main(int argc, char *argv[]) {
+    /* Mostrar mensaje de ayuda si el número de argumentos es incorrecto */
+    char *myself = basename(argv[0]);
+    if ((argc < 4) || (argc > 5)) {
+        printf("Uso: %s <iface> <mac> <dir> [<long>]\n", myself);
+        printf("       <iface>: Nombre de la interfaz Ethernet\n");
+        printf("        <tipo>: Campo 'Tipo' de las tramas Ethernet\n");
+        printf("         <mac>: Dirección MAC del servidor Ethernet\n");
+        printf("        <long>: Longitud de los datos enviados al servidor Ethernet\n");
+        printf("                (%d bytes por defecto)\n", DEFAULT_PAYLOAD_LENGTH);
+        exit(-1);
+    }
