9b2edcb3d6068dad06e0f96cba2a73799f501bb2
Divios <serberpar@gmail.com>
2020-12-02 09:19:05 +0100

    Fix a la declaracion de mensajes y entrada RIP

--- ripv2.h
+++ ripv2.h
@@ -7,22 +7,29 @@
 
 #include "udp.h"
 
+#define UNUSED 0x0000
+#define DEAFULT_FAMILY_DIRECTION 2
+#define RIPv2_TYPE_VERSION 2
+
+
 typedef struct entrada_rip{
 
+    uint16_t family_directions;
+    uint16_t route_label; //unused
     ipv4_addr_t subnet;
     ipv4_addr_t mask;
     ipv4_addr_t gw;
-    char iface[IFACE_NAME_MAX_LENGTH];
     int metric;
 
 } entrada_rip_t;
 
 typedef struct ripv2_msg{
-        // son definiciones todos estos campos, rellenar con su tipo correspondiente
-        int tipo;
-        int version;
-        int dominio_encaminamiento;
-        entrada_rip_t vectores_distancia[25];
+
+        uint8_t type;
+        uint8_t version;
+        uint16_t routing_domain; //unused
+        entrada_rip_t distance_vectors[25];
+
 } ripv2_msg_t;
 
 #endif //RYSCA_RIPV2_H
