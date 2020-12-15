c9343b1af5ea74302cd81afac0b16ad30ab947a3
Divios <serberpar@gmail.com>
2020-12-15 14:27:58 +0100

    Añadidos defines

--- ripv2.h
+++ ripv2.h
@@ -12,8 +12,11 @@
 #define UNUSED 0x0000
 #define DEAFULT_FAMILY_DIRECTION 2
 #define RIPv2_TYPE_VERSION 2
+#define RIPv2_REPLY 1
+#define RIPv2_REQUEST 2
 #define RIP_ROUTE_TABLE_SIZE 25
 #define RIP_PORT 520
+#define RIP_HEADER_SIZE 4
 
 
 typedef struct entrada_rip{
